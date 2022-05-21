3969de5a22058654979d2fb37c3c30dcce5115c2
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-05-21 10:59:17 +0500

    ...

--- js/script.js
+++ js/script.js
@@ -40,6 +40,58 @@ window.addEventListener("DOMContentLoaded",()=>{
       });
     }
   });
+
+  //modal
+
+  const allModalBtn = document.querySelectorAll("[data-modal]"),
+  modal = document.querySelector(".modal"),
+  modalClose = document.querySelector("[data-close");
+
+  
+allModalBtn.forEach(btn =>{
+      btn.addEventListener("click",()=>{
+        openModal()
+      })
+  })
+
+
+function openModal(){
+    modal.classList.add("show")
+    modal.classList.remove("hide")
+    document.body.style.overflow ="hidden"
+    clearInterval(modalTimer)
+}
+
+function closeModal(){
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+    document.body.style.overflow ="";
+}
+
+modalClose.addEventListener("click",()=>{
+    closeModal()
+})
+
+
+modal.addEventListener("click",(e)=>{
+if(e.target  === modal){
+    closeModal()
+}
+})
+
+const modalTimer  = setTimeout(openModal,5000)
+
+
+function showMyModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showMyModalByScroll);
+    }
+  }
+  window.addEventListener("scroll", showMyModalByScroll);
 })
 
 
