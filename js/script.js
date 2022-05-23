2f415d040b5cacb14df718fd81c72192bb4afa7b
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-05-23 16:53:36 +0500

    started

--- js/script.js
+++ js/script.js
@@ -145,5 +145,46 @@ function showMyModalByScroll() {
 
   setClock('.timer',dedline)
 
+
+  //CLASS
+
+  class CarCard{
+    constructor(src,alt,title,descr,price,parentSelector,...classess){
+      this.src = src;
+      this.alt = alt
+      this.title = title
+      this.descr = descr
+      this.classess = classess
+      this.parent = document.querySelector(parentSelector)
+      this.transfer = 10.500
+      this.changeTo()
+    }
+
+    changeTo(){
+      this.price = this.price * this.transfer;
+    }
+
+    render(){
+        const element = document.createElement("div");
+        element.innerHTML = `
+         <div class="menu__item">
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+              </div>
+              <div class="menu__item-divider"></div>
+              <div class="menu__item-price">
+              <div class="menu__item-cost">Price:</div>
+              <div class="menu__item-total"><span>${this.price}</span> $</div>
+            </div>
+          </div>
+        `
+        this.parent = append(element)
+    }
+
+  }
+
+  
 })
 
